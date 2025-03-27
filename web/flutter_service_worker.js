27041934af8221216b9b00122a9cee79ff76790a
Youssef Nabeh <yousseffe.nabeh1@gmail.com>
2025-03-27 21:29:42 +0200

    fix backend link

--- web/flutter_service_worker.js
+++ web/flutter_service_worker.js
@@ -340,12 +340,12 @@ const RESOURCES = {"assets/AssetManifest.bin": "d7891b4fa9149b3866ec33f702b6f960
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "4b2350e14c6650ba82871f60906437ea",
-"flutter_bootstrap.js": "200d0c717daf8092b85f4faa317dd609",
+"flutter_bootstrap.js": "18c9523ca253589a72e2eb74269161d8",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "index.html": "a526995c1bb1757579c9232dbc613897",
 "/": "a526995c1bb1757579c9232dbc613897",
-"main.dart.js": "b3a39bb8f41aa4085b2ed4f081f63c79",
+"main.dart.js": "66efe06239284dd1710a9de1c098198a",
 "manifest.json": "df8ee12e5d40a884cc23acd0588bda1c",
 "version.json": "e9eb58db72d407be27e9fa052224c304"};
 // The application shell files that are downloaded before a service worker can
